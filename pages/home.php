7b5d133e937f3bbbd16d1e2d5129472baa826b77
anad-zeal <elza.dana@gmail.com>
2025-10-04 12:45:09 -0600

    Fix typo in 'ouruniverse' in home.php

--- pages/home.php
+++ pages/home.php
@@ -5,7 +5,7 @@
 
     <p>
       <a href="/restoration?showSlideshow=true" class="dropcap" data-gallery="restoration" aria-label="Go to Restoration Projects">R</a>
-      estoration Projects — Instantaneous astral travel to any point in ouruniverse is now possible via the implementation of hyperbolic sub-resonant variations, combined with isomorphic silicon overlays manifesting dolor sit abet connecter advising elia.
+      estoration Projects — Instantaneous astral travel to any point in our universe is now possible via the implementation of hyperbolic sub-resonant variations, combined with isomorphic silicon overlays manifesting dolor sit abet connecter advising elia.
     </p>
 
     <p>
