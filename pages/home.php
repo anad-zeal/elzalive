31980dc0317397c30694f0551d70fdaa56973f4d
anad-zeal <elza.dana@gmail.com>
2025-10-04 11:31:44 -0600

    Fix typo in restoration projects description in home.php

--- pages/home.php
+++ pages/home.php
@@ -5,7 +5,8 @@
 
         <p>
             <a href="/restoration?showSlideshow=true" class="dropcap" data-gallery="restoration"
-                aria-label="Go to Restoration Projects">R</a>estoration Projects — DOGs Astral travel to any point in the
+                aria-label="Go to Restoration Projects">R</a>estoration Projects — Astral DOGs travel to any point in
+            the
             universe is now possible via the implementation of
             hyperbolic sub-resonant variations, combined with isomorphic silicon overlays manifesting dolor sit amet
             consectetur adipiscing elit.
