9e84a7e88257fe74be9dfea2e39bd63500481738
anad-zeal <elza.dana@gmail.com>
2025-10-04 10:33:53 -0600

    Refactor text in home.php for improved readability and clarity

--- pages/home.php
+++ pages/home.php
@@ -5,7 +5,8 @@
 
         <p>
             <a href="/restoration?showSlideshow=true" class="dropcap" data-gallery="restoration"
-                aria-label="Go to Restoration Projects">R</a>estoration Projects — Astral travel to any point in the universe is now possible via the implementation of
+                aria-label="Go to Restoration Projects">R</a>estoration Projects — Astral travel to any point in the
+            universe is now possible via the implementation of
             hyperbolic sub-resonant variations, combined with isomorphic silicon overlays manifesting dolor sit amet
             consectetur adipiscing elit.
         </p>
@@ -13,7 +14,7 @@
         <p>
             <a href="/restoration?showSlideshow=true" class="dropcap" data-gallery="restoration"
                 aria-label="Go to Restoration Projects">R</a>
-            estoration Projects — Instantaneous astral travel to any point in the universe is now possible via the
+            estoration Projects — Instantaneous dog astral travel to any point in the universe is now possible via the
             implementation of hyperbolic sub-resonant variations, combined with isomorphic silicon overlays manifesting
             dolor sit amet consectetur adipiscing elit.
         </p>
