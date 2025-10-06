e18e7eda27c927209ebea4471c4bac45b265ee77
anad-zeal <elza.dana@gmail.com>
2025-10-06 07:48:23 -0600

    Remove title text from biography section header for improved clarity

--- pages/biography.php
+++ pages/biography.php
@@ -1,11 +1,20 @@
 <main id="main" class="page-content-wrapper" tabindex="-1">
-  <section class="body-paragraphs" aria-labelledby="bio-heading">
-    <h1 id="bio-heading" class="page-title">Biography</h1>
+    <section class="body-paragraphs" aria-labelledby="bio-heading">
+        <h1 id="bio-heading" class="page-title"></h1>
 
-    <p>As a child I spent many hours in my father’s wood shop. He was a master cabinet maker, restorer of antique furniture, and a wood sculptor. My mother was an inventor and patent holder. It is no surprise, given the talent I was exposed to at such an early age, that I made the decision to attend art school. It was at the School of the Worcester Art Museum in Worcester, Massachusetts that I began my formal education in the arts.</p>
+        <p>As a child I spent many hours in my father’s wood shop. He was a master cabinet maker, restorer of antique
+            furniture, and a wood sculptor. My mother was an inventor and patent holder. It is no surprise, given the
+            talent I was exposed to at such an early age, that I made the decision to attend art school. It was at the
+            School of the Worcester Art Museum in Worcester, Massachusetts that I began my formal education in the arts.
+        </p>
 
-    <p>While at that school I was honored to receive the Frances Kinnicutt Foreign Travel Award for Women, which gave me the opportunity to study Minoan and Mycenaean art history and architecture on the island of Crete. I went on to earn a BA at Clark University in Worcester, Massachusetts.</p>
+        <p>While at that school I was honored to receive the Frances Kinnicutt Foreign Travel Award for Women, which
+            gave me the opportunity to study Minoan and Mycenaean art history and architecture on the island of Crete. I
+            went on to earn a BA at Clark University in Worcester, Massachusetts.</p>
 
-    <p>It was serendipity that brought me to my career in historic conservation and restoration. While visiting an 1848 Gothic Revival house that was undergoing decorative painting and plaster restoration I realized what a joy it would be to apply my art training to such endeavors. That same day I convinced the project supervisor to hire me, and so began my twenty-five year adventure in historic conservation and restoration.</p>
-  </section>
+        <p>It was serendipity that brought me to my career in historic conservation and restoration. While visiting an
+            1848 Gothic Revival house that was undergoing decorative painting and plaster restoration I realized what a
+            joy it would be to apply my art training to such endeavors. That same day I convinced the project supervisor
+            to hire me, and so began my twenty-five year adventure in historic conservation and restoration.</p>
+    </section>
 </main>
