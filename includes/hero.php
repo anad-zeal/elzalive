4274540e97673c5c76d234531770c2d1ef8057bd
anad-zeal <elza.dana@gmail.com>
2025-10-06 12:27:44 -0600

    Fix typo in hero section header class name

--- includes/hero.php
+++ includes/hero.php
@@ -1,5 +1,5 @@
 <main id="main" tabindex="-1">
     <section class="hero">
         <!--<h1 class="title">Alexis Elza</h1>-->
-        <h1 class="the=life">The life of an artist</h1>
+        <h1 class="the-life">The life of an artist</h1>
     </section>
