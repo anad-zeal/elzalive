7806646788cd1e7aa75de055d6395dadc1e1446b
anad-zeal <elza.dana@gmail.com>
2025-10-07 16:23:05 -0600

    Fix typo in hero section sub-title for consistency

--- includes/hero.php
+++ includes/hero.php
@@ -1,4 +1,4 @@
 <main id="main" tabindex="-1">
     <section class="hero">
-        <h1>class="sub-title">The life sof an artist</h1>
+        <h1 class="sub-title">The life sof an artist</h1>
     </section>
