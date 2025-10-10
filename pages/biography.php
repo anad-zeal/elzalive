77f5bd8af4caccc0916a7acdf3aac385229398ec
anad-zeal <elza.dana@gmail.com>
2025-10-09 21:45:38 -0600

    Remove duplicate h1 tag in biography section for consistency

--- pages/biography.php
+++ pages/biography.php
@@ -1,5 +1,6 @@
+<h1 id="biography-page-heading" class="page-title">Biography</h1>
 <section class="body-paragraphs" aria-labelledby="biography-page">
-    <h1 id="biography-page-heading" class="page-title">Biography</h1>
+
     <p>The woodsman saved the day by capturing the robber.</p>
 
 
