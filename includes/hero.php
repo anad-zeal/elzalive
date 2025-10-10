be45c6c97c2893daa36ab576268f2148c316ba3f
anad-zeal <elza.dana@gmail.com>
2025-10-09 21:25:15 -0600

    Update h1.title font-weight for improved visual hierarchy and correct title casing

--- includes/hero.php
+++ includes/hero.php
@@ -1,4 +1,4 @@
 <main id="main" tabindex="-1">
     <section class="hero">
-        <h1 class="title">The life of an artist</h1>
+        <h1 class="title">The Life of an Artist</h1>
     </section>
