3516251f4d6056c8d629b5d87ce89fbe0531d7a4
anad-zeal <elza.dana@gmail.com>
2025-10-09 20:55:36 -0600

    Update hero section heading to use consistent font-family styling

--- includes/hero.php
+++ includes/hero.php
@@ -1,4 +1,4 @@
 <main id="main" tabindex="-1">
     <section class="hero">
-        <h1 class="var(font-family-title)">The life of an artist</h1>
+        <h1>The life of an artist</h1>
     </section>
