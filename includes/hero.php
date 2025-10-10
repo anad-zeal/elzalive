5d7aaa0a7d6b07b469d0d42418e04702a88004db
anad-zeal <elza.dana@gmail.com>
2025-10-10 16:05:59 -0600

    Refactor footer and hero sections: move <main> tag to hero.php and add closing tag in footer.php

--- includes/hero.php
+++ includes/hero.php
@@ -1,3 +1,4 @@
-<section class="hero">
-    <h1 class="title">The Life of an Artist</h1>
-</section>
+<main id="main" tabindex="-1">
+    <section class="hero">
+        <h1 class="title">The Life of an Artist</h1>
+    </section>
