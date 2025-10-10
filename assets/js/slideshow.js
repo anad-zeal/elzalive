eda9e405401a797c796cf5811d98fc603a715f2a
anad-zeal <elza.dana@gmail.com>
2025-10-10 16:40:16 -0600

    Refactor slideshow module: remove export from Slideshow class and initSlideshows function for internal use

--- assets/js/slideshow.js
+++ assets/js/slideshow.js
@@ -40,7 +40,7 @@ function injectCoreStylesOnce(fadeMs = 1500) {
   document.head.appendChild(s);
 }
 
-export class Slideshow {
+class Slideshow {
   constructor(rootEl, opts = {}) {
     if (!rootEl) throw new Error('Slideshow root element is required.');
     this.root = rootEl;
@@ -264,7 +264,7 @@ export class Slideshow {
   }
 }
 
-export function initSlideshows(root = document) {
+function initSlideshows(root = document) {
   const nodes = [...root.querySelectorAll('[data-slides]')];
   return nodes
     .map((el) => {
