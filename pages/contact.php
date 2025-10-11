46c0b78cb05640270f05e83398e6da1dc961146f
anad-zeal <elza.dana@gmail.com>
2025-10-11 13:31:55 -0600

    Fix CSS variable declaration for font-family-title and remove redundant heading in contact page

--- pages/contact.php
+++ pages/contact.php
@@ -1,4 +1,3 @@
 <section class="body-paragraphs" aria-labelledby="contact-page">
     <h1 id="contact-heading" class="page-title">Contact</h1>
-    <h1 class="big-title">Contact Page</h1>
 </section>
