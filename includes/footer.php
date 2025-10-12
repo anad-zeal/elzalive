420ba13ac58455f9851b445b5dd553813935995a
anad-zeal <elza.dana@gmail.com>
2025-10-12 10:25:09 -0600

    Refactor loading spinner markup in footer.php for improved structure

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,9 @@
 </div> <!-- Closes #dynamic-page-wrapper -->
 <!-- Optional loading spinner -->
-<div id="loading-spinner" style="display:none; text-align:center; padding:20px;">Loading content...</div>
+<div id="loading-spinner" style="display:none; text-align:center; padding:20px;">
+    <img src="images/misc-images/spinner.gif" alt="Loading..." style="width:50px; height:50px;">
+    <p>Loading content...</p>
+</div>
 </main>
 
 <footer class="site-footer">
