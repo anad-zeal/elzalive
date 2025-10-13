18314f80e646c3661b25d46a6610ce6bed79e093
anad-zeal <elza.dana@gmail.com>
2025-10-12 21:30:06 -0600

    Update hero.php to correct title and subtitle structure for improved clarity

--- includes/hero.php
+++ includes/hero.php
@@ -1,3 +1,5 @@
+<main id="main" tabindex="-1">
     <section class="hero">
-        <h1 class="hero-title">The Life of an Artist</h1>
+        <h1 class="title">Alexis Elza</h1>
+        <p class="sub-title">The life of an artist</p>
     </section>
