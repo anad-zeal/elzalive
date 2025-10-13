a38ba6edd6be295054326928ae438eb4cbf4f10e
anad-zeal <elza.dana@gmail.com>
2025-10-13 10:43:52 -0600

    Refactor hero.php to remove unnecessary comments and streamline code structure for improved readability.

--- includes/hero.php
+++ includes/hero.php
@@ -3,6 +3,4 @@
     <!-- The subtitle for the current page will be updated by JS -->
     <p class="sub-title"><?= $page_title ?></p> <!-- Initial subtitle text for current page -->
 </section>
-<div id="dynamic-page-wrapper">
-    <!-- Initial content (from pages/home.php, pages/artworks.php, etc.) will be placed here on first load.
-                 Subsequent dynamic content loads will replace the innerHTML of this div. -->
+<div id="dynamic-page-wrapper"></div>
