52ec0d77342631c3f4b31d5b0f2a03494da1e10c
anad-zeal <elza.dana@gmail.com>
2025-10-14 15:45:49 -0600

    Refactor home page layout to use grid structure for improved responsiveness and organization

--- pages/home.php
+++ pages/home.php
@@ -1,17 +1,17 @@
-<!-- <section class="body-paragraphs" aria-labelledby="home-page-heading"> -->
-
-<p>
-    <a href="/restoration" class="category" data-gallery="restoration"
-        aria-label="Go to Restoration Projects">RestorationProjects</a>
-</p>
-
-<p>
-    <a href="/black-and-white" class="category" data-gallery="black-and-white" aria-label="Go to Black & White">Black &
-        White</a>
-</p>
-
-<p>
-    <a href="/artworks" class="category" data-gallery="artworks" aria-label="Go to Artworks">Artworks</a>
-</p>
-
-<!-- </section> -->
+<div class="grid-wrapper">
+    <div class="menu-grid-container">
+        <div class="menu-grid-item">
+            <a href="/restoration" class="category" data-gallery="restoration"
+                aria-label="Go to Restoration Projects">RestorationProjects</a>
+        </div>
+        <div class="menu-grid-item">
+            <a href="/black-and-white" class="category" data-gallery="black-and-white"
+                aria-label="Go to Black & White">Black &
+                White</a>
+        </div>
+        <div class="menu-grid-item">
+            <a href="/artworks" class="category" data-gallery="artworks" aria-label="Go to Artworks">Artworks</a>
+        </div>
+        <div class="menu-grid-item">Item 4</div>
+    </div>
+</div>
