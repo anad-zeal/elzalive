4e08f1805a1774ab4613c0796398781d90ac3ede
anad-zeal <elza.dana@gmail.com>
2025-10-14 12:21:16 -0600

    Fix indentation and update slideshow data source in black-and-white.php

--- pages/black-and-white.php
+++ pages/black-and-white.php
@@ -1,10 +1,9 @@
   <header class="page-header">
-    <div class="page-title-container">
-      <h1 class="page-title">Black &amp; White</h1>
-    </div>
+      <div class="page-title-container">
+          <h1 class="page-title">Black &amp; White</h1>
+      </div>
   </header>
   <section class="slideshow-section">
-    <div class="slideshow-container"
-         data-slides="/js/black-and-white-slides.json"
-         data-autoplay="true" data-interval="5000" data-fade="1500"></div>
+      <div class="slideshow-container" data-slides="/json-files/black-and-white-paintings.json" data-autoplay="true"
+          data-interval="5000" data-fade="1500"></div>
   </section>
