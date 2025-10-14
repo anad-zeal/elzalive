88dbe00ca2df889124985bc7020e2fe0a2a237d0
anad-zeal <elza.dana@gmail.com>
2025-10-14 06:31:13 -0600

    Uncomment paragraph in home.php to restore visibility of Restoration Projects link

--- pages/home.php
+++ pages/home.php
@@ -1,6 +1,6 @@
 <section class="body-paragraphs" aria-labelledby="home-page-heading">
 
-    <!-- <p>
+    <p>
         <a href="/restoration.php" class="dropcap" data-gallery="restoration"
             aria-label="Go to Restoration Projects">R</a>estoration
         Projects —
@@ -14,6 +14,6 @@
     <p>
         <a href="/artworks.php?showSlideshow=true" class="dropcap" data-gallery="artworks"
             aria-label="Go to Artworks">A</a>rtworks
-    </p> -->
+    </p>
 
 </section>
