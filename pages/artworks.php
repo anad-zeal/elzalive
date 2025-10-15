1538fd31e1c59a7733a0b76b4d976c5511b44c7f
anad-zeal <elza.dana@gmail.com>
2025-10-14 18:36:17 -0600

    Refactor artworks.php to improve code readability and maintainability

--- pages/artworks.php
+++ pages/artworks.php
@@ -1,23 +1,22 @@
-<section class="body-paragraphs" aria-labelledby="artworks-page-heading">
-    <h2 id="artworks-heading" class="visually-hidden">Artworks</h2>
+<!-- <section class="body-paragraphs" aria-labelledby="artworks-page-heading"> -->
+<h2 id="artworks-heading" class="visually-hidden">Artworks</h2>
 
-    <p>
-        <a href="/black-and-white" class="images" data-gallery="black_and_white_paintings"
-            aria-label="Go to Black and White Paintings">Black and White Paintings</a>
-    </p>
+<p>
+    <a href="/black-and-white" class="images" data-gallery="black_and_white_paintings"
+        aria-label="Go to Black and White Paintings">Black and White Paintings</a>
+</p>
 
-    <p><a href="/decorative" class="images" data-gallery="decorative_painting"
-            aria-label="Go to Decorative Painting">Decorative Painting</a>
-    </p>
+<p><a href="/decorative" class="images" data-gallery="decorative_painting"
+        aria-label="Go to Decorative Painting">Decorative Painting</a>
+</p>
 
-    <p>
-        <a href="/encaustic" class="images" data-gallery="encaustic_paintings"
-            aria-label="Go to Encaustic Paintings">Encaustic Paintings</a>
-    </p>
+<p>
+    <a href="/encaustic" class="images" data-gallery="encaustic_paintings"
+        aria-label="Go to Encaustic Paintings">Encaustic Paintings</a>
+</p>
 
-    <p>
-        <a href="/projects" class="images" data-gallery="project_series_paintings"
-            aria-label="Go to Project Series Paintings">Project Series Paintings</a>
-    </p>
-</section>
-<hr class="section-divider">
+<p>
+    <a href="/projects" class="images" data-gallery="project_series_paintings"
+        aria-label="Go to Project Series Paintings">Project Series Paintings</a>
+</p>
+<!-- </section> -->
