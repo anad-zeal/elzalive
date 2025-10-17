16275f2dfbb57786fe1235588d816b061996378b
anad-zeal <elza.dana@gmail.com>
2025-10-16 18:14:57 -0600

    Add category menu layout for artworks section; implement grid structure for improved organization and presentation.

--- pages/artworks.php
+++ pages/artworks.php
@@ -1,22 +1,20 @@
-<!-- <section class="body-paragraphs" aria-labelledby="artworks-page-heading"> -->
-<h2 id="artworks-heading" class="visually-hidden">Artworks</h2>
-
-<p>
-    <a href="/black-and-white" class="category images" data-gallery="black_and_white_paintings"
-        aria-label="Go to Black and White Paintings">Black and White Paintings</a>
-</p>
-
-<p><a href="/decorative" class="category images" data-gallery="decorative_painting"
-        aria-label="Go to Decorative Painting">Decorative Painting</a>
-</p>
-
-<p>
-    <a href="/encaustic" class="category images" data-gallery="encaustic_paintings"
-        aria-label="Go to Encaustic Paintings">Encaustic Paintings</a>
-</p>
-
-<p>
-    <a href="/projects" class="category images" data-gallery="project_series_paintings"
-        aria-label="Go to Project Series Paintings">Project Series Paintings</a>
-</p>
-<!-- </section> -->
+<!-- <h2 id="artworks-heading" class="visually-hidden">Artworks</h2> -->
+<div class="category-menu">
+    <div>
+        <class="decorative-painting">
+            <a href="/black-and-white" class="category images" data-gallery="black_and_white_paintings"
+                aria-label="Go to Black and White Paintings">Black and White Paintings</a>
+    </div>
+    <div class="encaustic-paintings">
+        <a href="/encaustic" class="category images" data-gallery="encaustic_paintings"
+            aria-label="Go to Encaustic Paintings">Encaustic Paintings</a>
+    </div>
+    <div class="project-se      ries-paintings">
+        <a href="/encaustic" class="category images" data-gallery="encaustic_paintings"
+            aria-label="Go to Encaustic Paintings">Encaustic Paintings</a>
+    </div>
+    <div class="black-and-white-paintings">
+        <a href="/projects" class="category images" data-gallery="project_series_paintings"
+            aria-label="Go to Project Series Paintings">Project Series Paintings</a>
+    </div>
+</div>
