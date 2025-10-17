5e51681854acc8dd2e96e575b158e15644e271d9
anad-zeal <elza.dana@gmail.com>
2025-10-17 11:31:28 -0600

    Refactor landing menu styles for improved layout and responsiveness

--- pages/artworks.php
+++ pages/artworks.php
@@ -1,19 +1,41 @@
 <!-- <h2 id="artworks-heading" class="visually-hidden">Artworks</h2> -->
 <section class="body-paragraphs" aria-labelledby="artworks-page-heading">
-    <div class="category-menu">
+    <div class="landing-menu">
         <div class="black-and-white"><a href="/black-and-white" class="category"
                 data-gallery="black_and_white_paintings" aria-label="Go to Black and White Paintings">Black and White
-                Paintings</a></div>
+                Paintings</a>
+            <p>Bruschetta soufflé mango chutney pancetta. Tiramisu radicchio simmered gnocchi flambé. Umami croissant
+                drizzle saffron aioli, marinate biscotti. Poached truffle risotto, caramelized shallots, fig compote.
+                Zest roux paprika, seared scallops with crème fraîche. Baste tempura</p>
+        </div>
+
+        <hr>
 
         <div class="encaustic"><a href="/encaustic" class="category" data-gallery="encaustic_paintings"
-                aria-label="Go to Encaustic Paintings">Encaustic Paintings</a></div>
+                aria-label="Go to Encaustic Paintings">Encaustic Paintings</a>
+            <p>Baste tempura, fennel purée, cocoa nibs. Sourdough béchamel, roasted chestnuts, lemon verbena glaze.
+                Infused gazpacho, heirloom tomatoes, miso broth. Pickled ramps, velvety mousse, charred leeks. Caviar
+                quenelle, pistachio brittle, braised lamb. Tarragon reduction, whipped mascarpone, blackberry coulis.
+            </p>
+        </div>
 
-        <div class="project-series"><a href="/project-series" class="category" data-gallery="project_series_paintings"
-                aria-label="Go to Project Series Paintings">Project Series Paintings</a></div>
+        <div class="project-series"><a href="/projects" class="category" data-gallery="project_series_paintings"
+                aria-label="Go to Project Series Paintings">Project Series Paintings</a>
+            <p> Sourdough béchamel, roasted chestnuts, lemon verbena glaze. Infused gazpacho, heirloom tomatoes, miso
+                broth. Pickled ramps, velvety mousse, charred leeks. Caviar quenelle, pistachio brittle, braised lamb.
+                Tarragon reduction, whipped mascarpone, blackberry coulis. Smoked gouda, candied pecans, ginger syrup.
+            </p>
+        </div>
 
         <div class="drips-series"><a href="/drips" class="category" data-gallery="drips_series_paintings"
-                aria-label="Go to Drips Series Paintings">Drips Series Paintings</a></div>
+                aria-label="Go to Drips Series Paintings">Drips Series Paintings</a>
+            <p>Caviar quenelle, pistachio brittle, braised lamb. Tarragon reduction, Poached truffle risotto,
+                caramelized shallots, fig compote. Zest roux paprika, seared scallops with crème fraîche. Baste tempura,
+                fennel purée, cocoa nibs. Sourdough béchamel, roasted chestnuts, lemon verbena glaze. Infused gazpacho.
+            </p>
+        </div>
     </div>
+
 
 
 </section>
