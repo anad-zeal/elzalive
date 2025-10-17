ef76fc9f7616ae5086fb1511d5bbb175d58c6dfa
anad-zeal <elza.dana@gmail.com>
2025-10-17 09:54:04 -0600

    Refactor category menu structure and styling for improved layout and accessibility

--- pages/artworks.php
+++ pages/artworks.php
@@ -1,14 +1,19 @@
 <!-- <h2 id="artworks-heading" class="visually-hidden">Artworks</h2> -->
-<div class="category-menu">
-    <div class="black-and-white"><a href="/black-and-white" class="category" data-gallery="black_and_white_paintings"
-            aria-label="Go to Black and White Paintings">Black and White Paintings</a></div>
+<section class="body-paragraphs" aria-labelledby="artworks-page-heading">
+    <div class="category-menu">
+        <div class="black-and-white"><a href="/black-and-white" class="category"
+                data-gallery="black_and_white_paintings" aria-label="Go to Black and White Paintings">Black and White
+                Paintings</a></div>
 
-    <div class="encaustic"><a href="/encaustic" class="category" data-gallery="encaustic_paintings"
-            aria-label="Go to Encaustic Paintings">Encaustic Paintings</a></div>
+        <div class="encaustic"><a href="/encaustic" class="category" data-gallery="encaustic_paintings"
+                aria-label="Go to Encaustic Paintings">Encaustic Paintings</a></div>
 
-    <div class="project-series"><a href="/projects" class="category" data-gallery="project_series_paintings"
-            aria-label="Go to Project Series Paintings">Project Series Paintings</a></div>
+        <div class="project-series"><a href="/projects" class="category" data-gallery="project_series_paintings"
+                aria-label="Go to Project Series Paintings">Project Series Paintings</a></div>
 
-    <div class="drips-series"><a href="/drips" class="category" data-gallery="drips_series_paintings"
-            aria-label="Go to Drips Series Paintings">Drips Series Paintings</a></div>
-</div>
+        <div class="drips-series"><a href="/drips" class="category" data-gallery="drips_series_paintings"
+                aria-label="Go to Drips Series Paintings">Drips Series Paintings</a></div>
+    </div>
+
+
+</section>
