081ae344e6217386e7735cc82b2e7655e8e68af3
anad-zeal <elza.dana@gmail.com>
2025-10-18 13:47:56 -0600

    Update font-title variable to use serif for improved typography

--- includes/header.php
+++ includes/header.php
@@ -71,8 +71,8 @@ function nav_item(string $slug, string $label, string $href): string
     <!-- Google Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Bonheur+Royale&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Playfair:ital,opsz,wght@0,5..1200,300..900;1,5..1200,300..900&display=swap"
+    <linkcss2?family=Bonheur+Royale&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..3
+        href="https://fonts.googleapis.com/2,100..900&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Playfair:ital,opsz,wght@0,5..1200,300..900;1,5..1200,300..900&display=swap"
         rel="stylesheet">
 
     <!-- Main Stylesheet -->
