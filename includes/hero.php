5ac097d12b96e210b1ef78417dc16fa7bec7084b
anad-zeal <elza.dana@gmail.com>
2025-10-18 11:36:00 -0600

    Comment out page title paragraphs in hero section for future use

--- includes/hero.php
+++ includes/hero.php
@@ -1,7 +1,7 @@
 <section class="hero">
     <h2 class="sub-title">The Life of an Artist</h2>
-    <p class="page-title"><?= $page_title ?></p>
-    <p class="page-title small-title"><?= $page_title ?></p>
+    <!-- <p class="page-title"><?= $page_title ?></p>
+    <p class="page-title small-title"><?= $page_title ?></p> -->
 </section>
 <div id="main-content-area">
     <!-- Initial page content will be inserted here by index.php -->
