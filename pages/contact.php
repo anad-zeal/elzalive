e39577e40a9fcc1581363000008579e0662ccf65
anad-zeal <elza.dana@gmail.com>
2025-10-18 13:05:57 -0600

    Add contact form with styling for improved user input experience

--- pages/contact.php
+++ pages/contact.php
@@ -1,3 +1,30 @@
 <section class="body-paragraphs" aria-labelledby="contact-page-heading">
+    <div class="wrapper">
+        <form method="post" action="" class="ccform">
+            <div class="ccfield-prepend">
+                <span class="ccform-addon"><i class="fa fa-user fa-2x"></i></span>
+                <input class="ccformfield" type="text" placeholder="YourName" required>
+            </div>
+            <div class="ccfield-prepend">
+                <span class="ccform-addon"><i class="fa fa-envelope fa-2x"></i></span>
+                <input class="ccformfield" type="text" placeholder="email" required>
+            </div>
+            <!-- <div class="ccfield-prepend">
+                <span class="ccform-addon"><i class="fa fa-mobile-phone fa-2x"></i></span>
+                <input class="ccformfield" type="text" placeholder="Phone">
+            </div> -->
+            <div class="ccfield-prepend">
+                <span class="ccform-addon"><i class="fa fa-info fa-2x"></i></span>
+                <input class="ccformfield" type="text" placeholder="Subject" required>
+            </div>
+            <div class="ccfield-prepend">
+                <span class="ccform-addon"><i class="fa fa-comment fa-2x"></i></span>
+                <textarea class="ccformfield" name="comments" rows="8" placeholder="Message" required></textarea>
+            </div>
+            <div class="ccfield-prepend">
+                <input class="ccbtn" type="submit" value="Submit">
+            </div>
+        </form>
+    </div>
 
 </section>
