c9f2d4615cce0e5836109ec562f0d523be40d81e
anad-zeal <elza.dana@gmail.com>
2025-10-19 19:49:07 -0600

    commit

--- pages/home.php
+++ pages/home.php
@@ -2,8 +2,7 @@
     <div class="landing-menu-item page"><a href="/decorative" class="page-link" data-gallery="decorative_painting"
             aria-label="Go to Decorative Painting">Decorative Painting</a>
         <p>Limit continuity differentiability derivative integral antiderivative chain rule product rule quotient rule
-            mean value theorem fundamental theorem of calculus Riemann sum definite integral indefinite integral partial
-            derivative gradient divergence curl Laplacian vector field scalar field.</p>
+            mean value theorem fundamental theorem of calculus.</p>
     </div>
 
     <div class="landing-menu-item page"><a href="/restoration" class="page-link" data-gallery="encaustic_paintings"
