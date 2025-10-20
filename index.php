b1f078757afd2c6299cd98ad52ef01e1d8a9a618
anad-zeal <elza.dana@gmail.com>
2025-10-19 20:11:45 -0600

    Fix incorrect file path for project series page in routing map

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ $map = [
     "black-and-white" => "pages/black-and-white.php",
     "drips" => "pages/drips.php",
     "encaustic" => "pages/encaustic.php",
-    "project-series" => "pages/projects.php",
+    "project-series" => "pages/project-series.php",
 ];
 
 // Resolve file or default to home
