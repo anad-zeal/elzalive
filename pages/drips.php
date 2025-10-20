a64f03d20806a57e5f8d5e49d6b89ddf8382ddc3
anad-zeal <elza.dana@gmail.com>
2025-10-20 09:42:12 -0600

    Fix typo in slideshow-container data-slides path in drips.php

--- pages/drips.php
+++ pages/drips.php
@@ -1,4 +1,4 @@
 <section class="slideshow-section">
-    <div class="slideshow-container" data-slides="assrts/images/drip-series-paintings.json" data-autoplay="true"
+    <div class="slideshow-container" data-slides="assets/images/drip-series-paintings.json" data-autoplay="true"
         data-interval="5000" data-fade="1500"></div>
 </section>
