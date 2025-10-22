b12c44471b0d3781c7e39fc2f62624a3a20710a5
anad-zeal <elza.dana@gmail.com>
2025-10-22 04:59:48 -0600

    Add script to rename files by replacing spaces with hyphens in specified directory
    - Implemented a Python script that renames files in a given folder by replacing spaces in filenames with hyphens.
    - Added error handling for folder access and file renaming operations.
    - Included user path expansion for compatibility with user directories.

--- pages/home.php
+++ pages/home.php
@@ -1,9 +1,11 @@
 <div class="page-menu">
     <div class="landing-menu-item page"><a href="/decorative" class="page-link" data-gallery="decorative_painting"
             aria-label="Go to Decorative Painting">Decorative Painting</a>
-        <p>Limit continuity differentiability derivative integral antiderivative chain rule product rule quotient rule
-            mean value theorem fundamental theorem of calculus. Equation inequality logarithm derivative integral
-            constant limit tangent cosine sine asymptote factor divisor</p>
+        <p>LDesire is the quiet architect of human behavior, and the orectic impulse—our innate drive toward what we
+            crave—shapes every facet of life. From the hunger that fuels survival to the yearning for connection,
+            beauty, or truth, orectic energy pulses beneath our choices, ambitions, and creations. It is not merely
+            appetite in the physical sense, but a broader motivational force that propels us toward fulfillment.
+        </p>
     </div>
 
     <div class="landing-menu-item page"><a href="/restoration" class="page-link" data-gallery="encaustic_paintings"
