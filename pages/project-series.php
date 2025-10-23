3366d741704005bfd8b69488e3ee320510f607fc
anad-zeal <elza.dana@gmail.com>
2025-10-23 16:53:56 -0600

    Add missing closing tag for slideshow section header

--- pages/project-series.php
+++ pages/project-series.php
@@ -1,4 +1,11 @@
 <section class="slideshow-section">
+    <div class="ss-header">
+        <div class="aep-logo">"<h2 class="sub-title">The Life of an Artist</h2>
+        </div>
+        <div class="ss-category">
+            <p class="page-title"></p>
+        </div>
+    </div>
     <div class="slideshow-container" data-slides="/json-files/project-series-paintings.json" data-autoplay="true"
         data-interval="5000" data-fade="1500"></div>
 </section>
