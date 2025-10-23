709093f285e66089c1921076a308c9adc39eb0e8
anad-zeal <elza.dana@gmail.com>
2025-10-23 16:59:10 -0600

    Refactor slideshow section: remove redundant header and category elements

--- includes/hero.php
+++ includes/hero.php
@@ -4,5 +4,13 @@
 
 </section>
 <div id="main-content-area">
+    <div class="ss-header">
+        <div class="aep-logo">
+            <h2 class="sub-title">The Life of an Artist</h2>
+        </div>
+        <div class="ss-category">
+            <p class="page-title"></p>
+        </div>
+    </div>
     <!-- Initial page content will be inserted here by index.php -->
     <!-- Dynamic content will be loaded here by navigation.js -->
