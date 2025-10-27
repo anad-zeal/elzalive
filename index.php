e68a56d0e560510f3c526bfd56aff82548bf1247
anad-zeal <elza.dana@gmail.com>
2025-10-27 15:57:51 -0600

    Refactor index.php: Update require statement to use $json_file for consistency

--- index.php
+++ index.php
@@ -4,5 +4,5 @@ $json_file;
 
 require __DIR__ . "/includes/header.php";
 require __DIR__ . "/includes/hero.php";
-require __DIR__ . "/$file";
+require __DIR__ . "/$json_file";
 require __DIR__ . "/includes/footer.php";
