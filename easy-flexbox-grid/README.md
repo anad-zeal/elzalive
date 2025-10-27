e1f3d5e0c60262b67c5563bd05cae56ead154799
anad-zeal <elza.dana@gmail.com>
2025-10-27 13:38:43 -0600

    eded

--- easy-flexbox-grid/README.md
+++
@@ -1,6 +0,0 @@
-# Easy Flexbox Grid
-
-A Pen created on CodePen.
-
-Original URL: [https://codepen.io/chriscoyier/pen/bedKLV](https://codepen.io/chriscoyier/pen/bedKLV).
-
--- includes/hero.php
+++
@@ -1,8 +0,0 @@
-<section class="hero">
-    <h2 class="sub-title">The Life of an Artist</h2>
-    <p class="page-title"></p>
-
-</section>
-<div id="main-content-area">
-    <!-- Initial page content will be inserted here by index.php -->
-    <!-- Dynamic content will be loaded here by navigation.js -->
