d756b290e2fc5b8fe1bd0ece80e1acd17fd36f99
anad-zeal <elza.dana@gmail.com>
2025-10-28 12:56:45 -0600

    Refactor footer.php: Comment out script inclusion for improved code clarity and maintainability

--- includes/footer.php
+++ includes/footer.php
@@ -7,7 +7,7 @@
     <p>&copy; 2025 elzalive • All rights reserved.</p>
 </footer>
 
-<script src="/assets/js/script.js"></script>
+<!-- <script src="/assets/js/script.js"></script> -->
 </body>
 
 </html>
