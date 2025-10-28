632a38620a2603d4a1d4031ad917a7197bc90fb2
anad-zeal <elza.dana@gmail.com>
2025-10-27 18:07:05 -0600

    Refactor index.php: Correct require statement to properly include $json_file

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <?php
 
-$json_file;
 
 require __DIR__ . "/includes/header.php";
 require __DIR__ . "/includes/hero.php";
-require __DIR__ . "/$json_file";
+require __DIR__ . $json_file;
 require __DIR__ . "/includes/footer.php";
