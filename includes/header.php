7cef919a8274a464ee882cbaf070efb68f102911
anad-zeal <elza.dana@gmail.com>
2025-10-31 14:44:05 -0600

    Add previous design assets in .afdesign and .svg formats
    - Introduced a new .afdesign file containing design elements for the project.
    - Added a corresponding .svg file with vector graphics for scalability and web use.

--- includes/header.php
+++ includes/header.php
@@ -59,6 +59,7 @@ $life_title = "<h2 class='sub-title fade-title'>The Life of an Artist</h2>";
 &family=Noto+Serif+Display:ital,wght@0,100..900;1,100..900
 &display=swap" rel="stylesheet">
 
+    <link rel="stylesheet" href="/assets/css/slideshow.css" />
     <link rel="stylesheet" href="/assets/css/style.css" />
 </head>
 
