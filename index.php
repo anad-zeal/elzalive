201b6a6049556bd60fac659e202321c275d1909f
anad-zeal <elza.dana@gmail.com>
2025-11-13 07:43:50 -0700

    Remove commented-out HTML structure from index.php

--- index.php
+++ index.php
@@ -1,26 +1,5 @@
-<!-- <!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <title>My Dynamic Slideshow</title>
-    <link rel="stylesheet" href="assets/css/style.css">
-</head>
-
-<body> -->
-
 <?php
 
 require __DIR__ . "/includes/header.php";
 require __DIR__ . "/includes/footer.php";
 require __DIR__ . "/includes/hero.php";
-?>
-
-
-
-
-// <?php
-
-// require __DIR__ . "/includes/header.php";
-//
-// require __DIR__ . "/includes/footer.php";
