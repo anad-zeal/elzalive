0ce2a62e76a9efaccca8eeeaa131ed958a04bfe8
anad-zeal <elza.dana@gmail.com>
2025-11-13 09:09:54 -0700

    Fix indentation in contents.php for better readability

--- includes/contents.php
+++ includes/contents.php
@@ -1,12 +1,12 @@
 <main id="dynamic-page-wrapper" data-page="home" tabindex="-1">
-    div id="slideshow-container">
-    <p>Loading images...</p>
+    <div id="slideshow-container">
+        <p>Loading images...</p>
 
-    <button id="prev-button" class="slide-nav">
-        <img src="assets/images/misc-images/prev.png" alt="Previous" />
-    </button>
-    <button id="next-button" class="slide-nav">
-        <img src="assets/images/misc-images/next.png" alt="Next" />
-    </button>
+        <button id="prev-button" class="slide-nav">
+            <img src="assets/images/misc-images/prev.png" alt="Previous" />
+        </button>
+        <button id="next-button" class="slide-nav">
+            <img src="assets/images/misc-images/next.png" alt="Next" />
+        </button>
     </div>
 </main>
