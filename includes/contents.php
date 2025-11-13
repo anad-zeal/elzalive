bf0781dddce222f7efdda2ea2fc842db72734b25
anad-zeal <elza.dana@gmail.com>
2025-11-13 09:20:12 -0700

    Refactor contents.php and hero.php to streamline structure; remove unnecessary main wrapper and update hero section for improved layout

--- includes/contents.php
+++ includes/contents.php
@@ -1,12 +1,10 @@
-<main id="dynamic-page-wrapper" data-page="home" tabindex="-1">
-    <div id="slideshow-container">
-        <p>Loading images...</p>
+<div id="slideshow-container">
+    <p>Loading images...</p>
 
-        <button id="prev-button" class="slide-nav">
-            <img src="assets/images/misc-images/prev.png" alt="Previous" />
-        </button>
-        <button id="next-button" class="slide-nav">
-            <img src="assets/images/misc-images/next.png" alt="Next" />
-        </button>
-    </div>
-</main>
+    <button id="prev-button" class="slide-nav">
+        <img src="assets/images/misc-images/prev.png" alt="Previous" />
+    </button>
+    <button id="next-button" class="slide-nav">
+        <img src="assets/images/misc-images/next.png" alt="Next" />
+    </button>
+</div>
--- includes/hero.php
+++ includes/hero.php
@@ -1,1 +1,10 @@
-<h1>AE Paints Slideshow</h1>
+section class="hero">
+<h2 class="sub-title">The Life of an Artist</h2>
+<p class="page-title"></p>
+</section>
+
+<main id="dynamic-page-wrapper" data-page="home" tabindex="-1">
+    <div id="dynamic-content-area">
+        <!-- Dynamic content will be loaded here by script.js -->
+    </div>
+</main>
