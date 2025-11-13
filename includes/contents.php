26ac1c8620cc035f51e5b3c1e481f83c177719c5
anad-zeal <elza.dana@gmail.com>
2025-11-13 09:07:05 -0700

    Add dynamic content structure to contents.php and create home.json for card grid data

--- includes/contents.php
+++ includes/contents.php
@@ -1,5 +1,12 @@
 <main id="dynamic-page-wrapper" data-page="home" tabindex="-1">
-    <div id="dynamic-content-area">
-        <h1 class="dca">Dynamic content will be loaded here by script.js </h1>
+    div id="slideshow-container">
+    <p>Loading images...</p>
+
+    <button id="prev-button" class="slide-nav">
+        <img src="assets/images/misc-images/prev.png" alt="Previous" />
+    </button>
+    <button id="next-button" class="slide-nav">
+        <img src="assets/images/misc-images/next.png" alt="Next" />
+    </button>
     </div>
 </main>
