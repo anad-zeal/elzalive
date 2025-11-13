64106d917a1213f442c3f60013f6dbc235bdcdbc
anad-zeal <elza.dana@gmail.com>
2025-11-13 08:53:34 -0700

    Refactor contents.php to remove slideshow and restore main structure; add slideshow-component.php for dedicated slideshow functionality

--- includes/contents.php
+++ includes/contents.php
@@ -1,10 +1,5 @@
-<div id="slideshow-container">
-    <p>Loading images...</p>
-
-    <button id="prev-button" class="slide-nav">
-        <img src="assets/images/misc-images/prev.png" alt="Previous" />
-    </button>
-    <button id="next-button" class="slide-nav">
-        <img src="assets/images/misc-images/next.png" alt="Next" />
-    </button>
-</div>
+<main id="dynamic-page-wrapper" data-page="home" tabindex="-1">
+    <div id="dynamic-content-area">
+        <p>Dynamic content will be loaded here by script.js </p>
+    </div>
+</main>
