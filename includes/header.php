f65ba1cfcd82f440c516924348fd23259f4d6081
anad-zeal <elza.dana@gmail.com>
2025-11-13 09:54:45 -0700

    Add slideshow functionality with dedicated CSS and JSON structure

--- includes/header.php
+++ includes/header.php
@@ -58,6 +58,7 @@ $life_title = "<h2 class='sub-title fade-title'>The Life of an Artist</h2>";
 
      <!-- Stylesheets -->
      <link rel="stylesheet" href="/assets/css/style.css" />
+     <link rel="stylesheet" href="/assets/css/slideshow.css" />
 
  </head>
 
