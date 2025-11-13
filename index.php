5f55a9be71916b7035cd0de7569a34a3d5d760d6
anad-zeal <elza.dana@gmail.com>
2025-11-13 01:15:31 -0700

    Remove HTML structure from index.php to streamline the file for PHP includes

--- index.php
+++ index.php
@@ -1,26 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <title>My Dynamic Slideshow</title>
-    <link rel="stylesheet" href="assets/css/style.css">
-</head>
-
-<body>
-
-    <h1>My Awesome Slideshow</h1>
-
-    <div id="slideshow-container">
-        <!-- The slideshow is loaded here by JavaScript -->
-        <p>Loading images...</p>
-    </div>
-
-    <script src="assets/js/slideshow.js"></script>
-
-</body>
-
-</html>
+<?php
 
 require __DIR__ . "/includes/header.php";
 require __DIR__ . "/includes/hero.php";
