c77b3c1841843a76167e813e4a98d6681084671b
anad-zeal <elza.dana@gmail.com>
2025-11-14 05:16:32 -0700

    Remove slideshow controls from contents.php for a cleaner layout

--- includes/contents.php
+++ includes/contents.php
@@ -1,17 +1,4 @@
  <main id="main-content">
-
-     <div class="slideshow-controls">
-         <label for="folder-selector">Select a Gallery:</label>
-         <select id="folder-selector">
-             <option value="drip-series-paintings">Drip Series</option>
-             <option value="black-and-white-paintings">Black and White</option>
-             <option value="decorative-painting">Decorative Painting</option>
-             <option value="encaustic-paintings">Encaustic Paintings</option>
-             <option value="project-series-paintings">Project Series</option>
-             <option value="historic-preservation">Historic Preservation</option>
-         </select>
-     </div>
-
      <div id="slideshow-container">
          <p>Loading Slideshow...</p>
          <button id="prev-button" class="slide-nav">
