7197cabf1198ff7608f05a44d63ca5759de2a0b4
anad-zeal <elza.dana@gmail.com>
2025-11-13 20:20:18 -0700

    TEMPORARY COMMIT MESSAGE

--- index.php
+++ index.php
@@ -1,13 +1,5 @@
-<?php include 'includes/header.php'; ?>
+<?php
 
-<!--
-    THIS IS THE CRUCIAL ELEMENT.
-    It must have the id="main-content".
-    The JavaScript engine will inject all page content here.
--->
-<main id="main-content">
-    <!-- The initial loading message can live here or be added by JS -->
-    <p class="loading-message">Loading...</p>
-</main>
-
-<?php include 'includes/footer.php'; ?>
+require __DIR__ . "/includes/header.php";
+require __DIR__ . "/includes/contents.php";
+require __DIR__ . "/includes/footer.php";
