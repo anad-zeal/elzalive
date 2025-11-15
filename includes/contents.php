c867d112cb19a8438b5eea3737548fe8872f4f4d
anad-zeal <elza.dana@gmail.com>
2025-11-15 07:00:00 -0700

    refactor slideshow structure and restore navigation buttons

--- includes/contents.php
+++ includes/contents.php
@@ -1,29 +1,12 @@
- <main id="main-content">
-     <div id="slideshow-container">
-         <div class="slideshow-controls">
-             <label for="folder-selector">Select a Gallery:</label>
-             <select id="folder-selector">
-                 <option value="drip-series-paintings">Drip Series</option>
-                 <option value="black-and-white-paintings">Black and White</option>
-                 <option value="decorative-painting">Decorative Painting</option>
-                 <option value="encaustic-paintings">Encaustic Paintings</option>
-                 <option value="project-series-paintings">Project Series</option>
-                 <option value="historic-preservation">Historic Preservation</option>
-             </select>
-         </div>
+<main id="main-content">
+    <div id="slideshow-container">
+        <p>Loading Slideshow...</p>
+        <button id="prev-button" class="slide-nav">
+            <img src="images/misc-images/prev.png" alt="Previous" />
 
-     </div>
- </main>
-
- <!-- <main id="main-content">
-     <div id="slideshow-container">
-         <p>Loading Slideshow...</p>
-         <button id="prev-button" class="slide-nav">
-             <img src="images/misc-images/prev.png" alt="Previous" />
-
-         </button>
-         <button id="next-button" class="slide-nav">
-             <img src="images/misc-images/next.png" alt="Next" />
-         </button>
-     </div>
- </main> -->
+        </button>
+        <button id="next-button" class="slide-nav">
+            <img src="images/misc-images/next.png" alt="Next" />
+        </button>
+    </div>
+</main>
