103eeb4d974062d445882776fbfdd7106562ebf2
anad-zeal <elza.dana@gmail.com>
2025-11-15 08:29:09 -0700

    update menu.php: correct option value for decorative painting in folder selector

--- includes/menu.php
+++ includes/menu.php
@@ -3,7 +3,7 @@
      <select id="folder-selector">
          <option value="drip-series-paintings">Drip Series</option>
          <option value="black-and-white-paintings">Black and White</option>
-         <option value="decorative-painting">Decorative Painting</option>
+         <option value="decorative-slideshow">Decorative Painting</option>
          <option value="encaustic-paintings">Encaustic Paintings</option>
          <option value="project-series-paintings">Project Series</option>
          <option value="historic-preservation">Historic Preservation</option>
