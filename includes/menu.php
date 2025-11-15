2a30aebcc5df5ef37ba2a2a2c7ffa8b5750e1944
anad-zeal <elza.dana@gmail.com>
2025-11-15 07:37:58 -0700

    refactor menu structure: replace old menu with new dropdown selection for galleries

--- includes/menu.php
+++ includes/menu.php
@@ -1,8 +1,11 @@
-<nav id="folder-selector">
-    <a class="link-3" data-page="decorative-painting" href="#">Decorative</a>
-    <a class="link-3" data-page="drip-series-paintings" href="#">Drip Series</a>
-    <a class="link-3" data-page="black-and-white-paintings" href="#">Black and White</a>
-    <a class="link-3" data-page="decorative-painting" href="#">Decorative Painting</a>
-    <a class="link-3" data-page="project-series-paintings" href="#">Project Series</a>
-    <a class="link-3" data-page="historic-preservation" href="#">Historic Preservation</a>
-</nav>
+ <div class="slideshow-controls">
+     <label for="folder-selector">Select a Gallery:</label>
+     <select id="folder-selector">
+         <option value="drip-series-paintings">Drip Series</option>
+         <option value="black-and-white-paintings">Black and White</option>
+         <option value="decorative-painting">Decorative Painting</option>
+         <option value="encaustic-paintings">Encaustic Paintings</option>
+         <option value="project-series-paintings">Project Series</option>
+         <option value="historic-preservation">Historic Preservation</option>
+     </select>
+ </div>
