d7711e06ecb42133472d6d1d2a6ad3a829b14b45
anad-zeal <elza.dana@gmail.com>
2025-11-15 08:02:04 -0700

    update slideshow: comment out loading message and navigation buttons for clarity

--- includes/contents.php
+++ includes/contents.php
@@ -1,12 +1,12 @@
 <main id="main-content">
     <div id="slideshow-container">
-        <p>Loading Slideshow...</p>
+        <!-- <p>Loading Slideshow...</p>
         <button id="prev-button" class="slide-nav">
             <img src="images/misc-images/prev.png" alt="Previous" />
 
         </button>
         <button id="next-button" class="slide-nav">
             <img src="images/misc-images/next.png" alt="Next" />
-        </button>
+        </button> -->
     </div>
 </main>
