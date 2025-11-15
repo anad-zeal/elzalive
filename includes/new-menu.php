f88c42bc966f7b070ad260a927259216c0cd70dd
anad-zeal <elza.dana@gmail.com>
2025-11-15 05:34:11 -0700

    update navigation menu to include additional painting categories

--- includes/new-menu.php
+++ includes/new-menu.php
@@ -1,6 +1,14 @@
-<nav id="nav-3">
-    <a class="link-3" data-page="drip-series-paintings" href="#">Decorative</a>
+<!-- <nav id="nav-3">
+    <a class="link-3" <a class="link-3" data-page="drip-series-paintings" href="#">Decorative</a>
     <a class="link-3" href="#">About</a>
     <a class="link-3" href="#">Contact</a>
     <a class="link-3" href="#">Shop</a>
+</nav> -->
+<nav id="nav-3">
+    <a class="link-3" data-page="drip-series-paintings">Drip Series</a>
+    <a class="link-3" data-page="black-and-white-paintings">Black and White</a>
+    <a class="link-3" data-page="decorative-painting">Decorative Painting</a>
+    <a class="link-3" data-page="encaustic-paintings">Encaustic Paintings</a>
+    <a class="link-3" data-page="project-series-paintings">Project Series</a>
+    <a class="link-3" data-page="historic-preservation">Historic Preservation</a>
 </nav>
