d24d538efb1e8f2f1715d5cbb0fe240891552438
anad-zeal <elza.dana@gmail.com>
2025-11-14 21:04:33 -0700

    ABC

--- includes/menu.php
+++ includes/menu.php
@@ -1,11 +1,11 @@
  <div class="slideshow-controls">
-         <label for="folder-selector">Select a Gallery:</label>
-         <select id="folder-selector">
-             <option value="drip-series-paintings">Drip Series</option>
-             <option value="black-and-white-paintings">Black and White</option>
-             <option value="decorative-painting">Decorative Painting</option>
-             <option value="encaustic-paintings">Encaustic Paintings</option>
-             <option value="project-series-paintings">Project Series</option>
-             <option value="historic-preservation">Historic Preservation</option>
-         </select>
-     </div>
+     <label for="folder-selector">Select a Gallery:</label>
+     <select id="folder-selector">
+         <option value="drip-series-paintings">Drip Series</option>
+         <option value="black-and-white-paintings">Black and White</option>
+         <option value="decorative-painting">Decorative Painting</option>
+         <option value="encaustic-paintings">Encaustic Paintings</option>
+         <option value="project-series-paintings">Project Series</option>
+         <option value="historic-preservation">Historic Preservation</option>
+     </select>
+ </div>
---
+++ includes/new-menu.php
@@ -0,0 +1,7 @@
+
+<nav id="nav-3">
+  <a class="link-3" href="#">Home</a>
+  <a class="link-3" href="#">About</a>
+  <a class="link-3" href="#">Contact</a>
+  <a class="link-3" href="#">Shop</a>
+</nav>
