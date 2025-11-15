d9fcc1e076d6ca6db7e04e4878f48af90d0edb91
anad-zeal <elza.dana@gmail.com>
2025-11-15 07:34:19 -0700

    update menu inclusion: remove duplicate menu file reference

--- index.php
+++ index.php
@@ -2,6 +2,6 @@
 
 require __DIR__ . "/includes/header.php";
 require __DIR__ . "/includes/menu.php";
-require __DIR__ . "/includes/new-menu.php";
+require __DIR__ . "/includes/menu.php";
 //require __DIR__ . "/includes/contents.php";
 require __DIR__ . "/includes/footer.php";
