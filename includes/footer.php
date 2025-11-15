a9f4a4708ec68a3850c33d5b05d9f2cd6aa1eed0
anad-zeal <elza.dana@gmail.com>
2025-11-15 07:31:34 -0700

    update footer scripts: swap order of script inclusion and comment out old script

--- includes/footer.php
+++ includes/footer.php
@@ -2,8 +2,8 @@
     <p>&copy; 2025 elzalive • All rights reserved.</p>
 </footer>
 
-<script src="assets/js/script.js" defer></script>
-<!-- <script src="assets/js/new-menu-script.js" defer></script> -->
+<!-- <script src="assets/js/script.js" defer></script> -->
+<script src="assets/js/new-menu-script.js" defer></script>
 </body>
 
 </html>
