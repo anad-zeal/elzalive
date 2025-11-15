8bac7d8c62a89bde1ad60e51040df047db0b84e4
anad-zeal <elza.dana@gmail.com>
2025-11-15 06:51:16 -0700

    add slideshow controls and gallery selection to main content

--- includes/contents.php
+++ includes/contents.php
@@ -1,5 +1,22 @@
  <main id="main-content">
      <div id="slideshow-container">
+         <div class="slideshow-controls">
+             <label for="folder-selector">Select a Gallery:</label>
+             <select id="folder-selector">
+                 <option value="drip-series-paintings">Drip Series</option>
+                 <option value="black-and-white-paintings">Black and White</option>
+                 <option value="decorative-painting">Decorative Painting</option>
+                 <option value="encaustic-paintings">Encaustic Paintings</option>
+                 <option value="project-series-paintings">Project Series</option>
+                 <option value="historic-preservation">Historic Preservation</option>
+             </select>
+         </div>
+
+     </div>
+ </main>
+
+ <!-- <main id="main-content">
+     <div id="slideshow-container">
          <p>Loading Slideshow...</p>
          <button id="prev-button" class="slide-nav">
              <img src="images/misc-images/prev.png" alt="Previous" />
@@ -9,4 +26,4 @@
              <img src="images/misc-images/next.png" alt="Next" />
          </button>
      </div>
- </main>
+ </main> -->
