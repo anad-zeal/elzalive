56b533766144853dce8724dc8685076bcc8c0e56
anad-zeal <elza.dana@gmail.com>
2025-11-15 07:03:32 -0700

    comment out main content and slideshow structure for future use

--- includes/contents.php
+++ includes/contents.php
@@ -1,12 +1,12 @@
-<main id="main-content">
-    <div id="slideshow-container">
-        <p>Loading Slideshow...</p>
-        <button id="prev-button" class="slide-nav">
-            <img src="images/misc-images/prev.png" alt="Previous" />
+<!-- <main id="main-content">
+     <div id="slideshow-container">
+         <p>Loading Slideshow...</p>
+         <button id="prev-button" class="slide-nav">
+             <img src="images/misc-images/prev.png" alt="Previous" />
 
-        </button>
-        <button id="next-button" class="slide-nav">
-            <img src="images/misc-images/next.png" alt="Next" />
-        </button>
-    </div>
-</main>
+         </button>
+         <button id="next-button" class="slide-nav">
+             <img src="images/misc-images/next.png" alt="Next" />
+         </button>
+     </div>
+ </main> -->
