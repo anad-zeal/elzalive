b892191d59786f8743f6153076d9481cdb3c1503
anad-zeal <elza.dana@gmail.com>
2025-11-15 03:41:06 -0700

    update navigation menu to include 'Decorative' link

--- includes/new-menu.php
+++ includes/new-menu.php
@@ -1,7 +1,6 @@
-
 <nav id="nav-3">
-  <a class="link-3" href="#">Home</a>
-  <a class="link-3" href="#">About</a>
-  <a class="link-3" href="#">Contact</a>
-  <a class="link-3" href="#">Shop</a>
+    <a class="link-3" data-page="drip-series-paintings" href="#">Decorative</a>
+    <a class="link-3" href="#">About</a>
+    <a class="link-3" href="#">Contact</a>
+    <a class="link-3" href="#">Shop</a>
 </nav>
