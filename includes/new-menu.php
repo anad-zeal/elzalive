40d86c2af0f3c7101c6a17dba99c56db8d8ff440
anad-zeal <elza.dana@gmail.com>
2025-11-15 06:18:02 -0700

    update navigation menu to correct link structure and enhance accessibility

--- includes/new-menu.php
+++ includes/new-menu.php
@@ -4,11 +4,13 @@
     <a class="link-3" href="#">Contact</a>
     <a class="link-3" href="#">Shop</a>
 </nav> -->
+
+
 <nav id="nav-3">
-    <a class="link-3" data-page="drip-series-paintings">Drip Series</a>
-    <a class="link-3" data-page="black-and-white-paintings">Black and White</a>
-    <a class="link-3" data-page="decorative-painting">Decorative Painting</a>
-    <a class="link-3" data-page="encaustic-paintings">Encaustic Paintings</a>
-    <a class="link-3" data-page="project-series-paintings">Project Series</a>
-    <a class="link-3" data-page="historic-preservation">Historic Preservation</a>
+    <a class="link-3" data-page="decorative-painting" href="#">Decorative</a>
+    <a class="link-3" data-page="drip-series-paintings" href="#">Drip Series</a>
+    <a class="link-3" data-page="black-and-white-paintings" href="#">Black and White</a>
+    <a class="link-3" data-page="decorative-painting" href="#">Decorative Painting</a>
+    <a class="link-3" data-page="project-series-paintings" href="#">Project Series</a>
+    <a class="link-3" data-page="historic-preservation" href="#">Historic Preservation</a>
 </nav>
