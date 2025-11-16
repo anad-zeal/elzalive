6448af52cf4553ee408956006a40f3c45c574690
anad-zeal <elza.dana@gmail.com>
2025-11-16 15:44:24 -0700

    Refactor menu styles and HTML structure; remove unused navigation elements and consolidate CSS for improved maintainability. Add new slideshow menu HTML and CSS files with responsive design features and enhanced button interactions.

--- includes/contents.php
+++ includes/contents.php
@@ -1,25 +1,5 @@
 <main id="main-content">
-    <nav id="nav-1">
-        <a class="link-1" href="#">Home</a>
-        <a class="link-1" href="#">About</a>
-        <a class="link-1" href="#">Contact</a>
-        <a class="link-1" href="#">Shop</a>
-    </nav>
-
-    <nav id="nav-2">
-        <a class="link-2" href="#">Home</a>
-        <a class="link-2" href="#">About</a>
-        <a class="link-2" href="#">Contact</a>
-        <a class="link-2" href="#">Shop</a>
-    </nav>
-
-    <nav id="nav-3">
-        <a class="link-3" href="#">Home</a>
-        <a class="link-3" href="#">About</a>
-        <a class="link-3" href="#">Contact</a>
-        <a class="link-3" href="#">Shop</a>
-    </nav>
-    <!-- <div id="slideshow-container">
+    <div id="slideshow-container">
         <p>Loading Slideshow...</p>
         <button id="prev-button" class="slide-nav">
             <img src="images/misc-images/prev.png" alt="Previous" />
@@ -28,5 +8,5 @@
         <button id="next-button" class="slide-nav">
             <img src="images/misc-images/next.png" alt="Next" />
         </button>
-    </div> -->
+    </div>
 </main>
