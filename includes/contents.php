e2bf5cdf12c5d63a5e42f63fdca6ffe23371eceb
anad-zeal <elza.dana@gmail.com>
2025-11-16 08:59:17 -0700

    update(contents.php): restructure navigation elements and comment out slideshow container

--- includes/contents.php
+++ includes/contents.php
@@ -1,5 +1,25 @@
 <main id="main-content">
-    <div id="slideshow-container">
+    <nav id="nav-1">
+        <a class="link-1" href="#">Home</a>
+        <a class="link-1" href="#">About</a>
+        <a class="link-1" href="#">Contact</a>
+        <a class="link-1" href="#">Shop</a>
+    </nav>
+
+    <nav id="nav-2">
+        <a class="link-2" href="#">Home</a>
+        <a class="link-2" href="#">About</a>
+        <a class="link-2" href="#">Contact</a>
+        <a class="link-2" href="#">Shop</a>
+    </nav>
+
+    <nav id="nav-3">
+        <a class="link-3" href="#">Home</a>
+        <a class="link-3" href="#">About</a>
+        <a class="link-3" href="#">Contact</a>
+        <a class="link-3" href="#">Shop</a>
+    </nav>
+    <!-- <div id="slideshow-container">
         <p>Loading Slideshow...</p>
         <button id="prev-button" class="slide-nav">
             <img src="images/misc-images/prev.png" alt="Previous" />
@@ -8,5 +28,5 @@
         <button id="next-button" class="slide-nav">
             <img src="images/misc-images/next.png" alt="Next" />
         </button>
-    </div>
+    </div> -->
 </main>
