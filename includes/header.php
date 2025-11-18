1d62950a86b368ca42a45c7d43e2375adacf65fa
anad-zeal <elza.dana@gmail.com>
2025-11-17 18:16:57 -0700

    update(header.php): switch stylesheet from style-hover-lines.css to menu.css for consistency

--- includes/header.php
+++ includes/header.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Slideshow Gallery</title>
     <link rel="stylesheet" href="assets/css/style.css" />
-    <link rel="stylesheet" href="assets/css/style-hover-lines.css" />
+    <link rel="stylesheet" href="assets/css/menu.css" />
 
 </head>
 
