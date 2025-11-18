c78ae58d14d2f6a918ad0ad8979937adfabc03e5
anad-zeal <elza.dana@gmail.com>
2025-11-18 16:40:35 -0700

    eee

--- includes/header.php
+++ includes/header.php
@@ -56,7 +56,7 @@ $life_title = "<h2 class='sub-title fade-title'>The Life of an Artist</h2>";
          href="https://fonts.googleapis.com/css2?family=Bonheur+Royale&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Noto+Serif+Display:ital,wght@0,100..900;1,100..900&display=swap"
          rel="stylesheet">
 
-     <!-- Stylesheets -->
+     <!-- Stylesheets sssssss-->
      <link rel="stylesheet" href="/assets/css/style.css" />
      <link rel="stylesheet" href="/assets/css/slideshow.css" />
 
