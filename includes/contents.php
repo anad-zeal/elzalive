77e1f0b7ece492fc9396695a71d380ba9257762b
anad-zeal <elza.dana@gmail.com>
2025-11-17 21:00:22 -0700

    oops

--- includes/contents.php
+++ includes/contents.php
@@ -1,12 +1,14 @@
 <main id="main-content">
-    <div id="slideshow-container">
-        <p>Loading Slideshow...</p>
-        <button id="prev-button" class="slide-nav">
-            <img src="images/misc-images/prev.png" alt="Previous" />
+    <div id="slideshow-image-area">
+        <h1>Slideshow images will be dynamically loaded here</h1>
+        <div id="slideshow-container">
+            <p>Loading Slideshow...</p>
+            <button id="prev-button" class="slide-nav">
+                <img src="images/misc-images/prev.png" alt="Previous" />
 
-        </button>
-        <button id="next-button" class="slide-nav">
-            <img src="images/misc-images/next.png" alt="Next" />
-        </button>
-    </div>
+            </button>
+            <button id="next-button" class="slide-nav">
+                <img src="images/misc-images/next.png" alt="Next" />
+            </button>
+        </div>
 </main>
