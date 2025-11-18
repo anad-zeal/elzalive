deaf434ade62fa67ad3db95052b6f56bb375ec1b
anad-zeal <elza.dana@gmail.com>
2025-11-17 22:04:37 -0700

    fix: remove unused CSS and JS files, update header and footer includes

--- includes/header.php
+++ includes/header.php
@@ -6,8 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Slideshow Gallery</title>
 
-    <link rel="stylesheet" href="assets/css/menu.css" />
-    <link rel="stylesheet" href="assets/css/slideshow.css" />
+    <link rel="stylesheet" href="assets/css/style.css" />
 
 </head>
 
