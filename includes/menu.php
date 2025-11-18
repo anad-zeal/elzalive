fc570e9ab3986b4fd136db167e289705ba378e51
anad-zeal <elza.dana@gmail.com>
2025-11-18 16:26:09 -0700

    fix: remove commented-out gallery menu code; clean up menu.php

--- includes/menu.php
+++ includes/menu.php
@@ -6,12 +6,3 @@
     <a class="link-3" data_page="project-series-paintings" href="#">Project Series</a>
     <a class="link-3" data_page="historic-preservation" href="#">Historic Preservation</a>
 </nav>
-
-<!-- <div class="gallery-menu">
-    <button class="col logo"><img src="https://aepaints.com/wp-content/uploads/logo-aepaints.svg" width="300"
-            class="logo-class" /></button>
-    <button class="col btn idx-1" data-folder="black-and-white">Black and White</button>
-    <button class="col btn" data-folder="drips">Drips</button>
-    <button class="col btn" data-folder="encaustic">Encaustic</button>
-    <button class="col btn" data-folder="projects">Projects</button>
-</div> -->
