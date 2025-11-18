5bd74259ee44a807baa57083f1b7533cf3d149a2
anad-zeal <elza.dana@gmail.com>
2025-11-18 16:22:02 -0700

    feat: add gallery menu with buttons for different painting categories; enhance navigation

---
+++ includes/menu.php
@@ -0,0 +1,17 @@
+<!-- <nav id="folder-selector">
+    <a class="link-3" data_page="decorative-painting" href="#">Decorative</a>
+    <a class="link-3" data_page="drip-series-paintings" href="#">Drip Series</a>
+    <a class="link-3" data_page="black-and-white-paintings" href="#">Black and White</a>
+    <a class="link-3" data_page="decorative-painting" href="#">Decorative Painting</a>
+    <a class="link-3" data_page="project-series-paintings" href="#">Project Series</a>
+    <a class="link-3" data_page="historic-preservation" href="#">Historic Preservation</a>
+</nav> -->
+
+<div class="gallery-menu">
+    <button class="col logo"><img src="https://aepaints.com/wp-content/uploads/logo-aepaints.svg" width="300"
+            class="logo-class" /></button>
+    <button class="col btn idx-1" data-folder="black-and-white">Black and White</button>
+    <button class="col btn" data-folder="drips">Drips</button>
+    <button class="col btn" data-folder="encaustic">Encaustic</button>
+    <button class="col btn" data-folder="projects">Projects</button>
+</div>
