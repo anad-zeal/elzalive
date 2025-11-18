956fbd4aa9c4a0c8f0ff6b5c44ccb7b6dd03b834
anad-zeal <elza.dana@gmail.com>
2025-11-18 16:16:46 -0700

    fix: remove unused script reference from footer; streamline codebase

--- includes/footer.php
+++ includes/footer.php
@@ -2,7 +2,6 @@
     <p>&copy; 2025 elzalive • All rights reserved.</p>
 </footer>
 
-<script src="assets/js/script.js" defer></script>
 <script src="assets/js/slideshow.js" defer></script>
 
 </body>
