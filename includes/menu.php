260ac1b8fc8e83dd6ac7c96e036472431b9cfaf9
anad-zeal <elza.dana@gmail.com>
2025-11-17 19:08:20 -0700

    update(menu.php): replace slideshow controls with a simplified navigation menu

--- includes/menu.php
+++ includes/menu.php
@@ -1,24 +1,8 @@
- <!-- <div class="slideshow-controls">
-     <label for="folder-selector">Select a Gallery:</label>
-     <select id="folder-selector">
-         <option value="drip-series-paintings">Drip Series</option>
-         <option value="black-and-white-paintings">Black and White</option>
-         <option value="decorative-painting">Decorative Painting</option>
-         <option value="encaustic-paintings">Encaustic Paintings</option>
-         <option value="project-series-paintings">Project Series</option>
-         <option value="historic-preservation">Historic Preservation</option>
-     </select>
- </div> -->
-
- <!-- 11 -->
-
-
- <div class="slideshow-controls">
-     <nav id="folder-selector" class="gallery-menu">>
-         <!-- <button class="col logo"><img src="https://aepaints.com/wp-content/uploads/logo-aepaints.svg" width="300" class="logo-class"/></button> -->
-         <button class="col btn idx-1" data-folder="black-and-white">Black and White</button>
-         <button class="col btn" data-folder="drips">Drips</button>
-         <button class="col btn" data-folder="encaustic">Encaustic</button>
-         <button class="col btn" data-folder="projects">Projects</button>
-     </nav>
- </div>
+<nav id="folder-selector">
+    <a class="link-3" data_page="decorative-painting" href="#">Decorative</a>
+    <a class="link-3" data_page="drip-series-paintings" href="#">Drip Series</a>
+    <a class="link-3" data_page="black-and-white-paintings" href="#">Black and White</a>
+    <a class="link-3" data_page="decorative-painting" href="#">Decorative Painting</a>
+    <a class="link-3" data_page="project-series-paintings" href="#">Project Series</a>
+    <a class="link-3" data_page="historic-preservation" href="#">Historic Preservation</a>
+</nav>
