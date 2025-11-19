f05cc83848e10975caf152c4e4acbc76b24f7c26
anad-zeal <elza.dana@gmail.com>
2025-11-18 17:27:58 -0700

    fix: update script source in footer.php to correct slideshow functionality

--- includes/footer.php
+++ includes/footer.php
@@ -2,7 +2,7 @@
     <p>&copy; 2025 elzalive • All rights reserved.</p>
 </footer>
 
-<script src="assets/js/slideshow.js" defer></script>
+<script src="assets/js/sscript.js" defer></script>
 
 </body>
 
