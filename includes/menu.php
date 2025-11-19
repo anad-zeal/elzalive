9bbd1d15c1b2d892900380730de8bb2a204ac31b
anad-zeal <elza.dana@gmail.com>
2025-11-19 08:52:36 -0700

    fix: update link text for decorative painting in menu.php for clarity

--- includes/menu.php
+++ includes/menu.php
@@ -1,5 +1,5 @@
 <nav id="folder-selector">
-    <a class="link-3" data_page="decorative-painting" href="#">Decorative</a>
+    <a class="link-3" data_page="decorative-painting" href="#">Decorative Painting</a>
     <a class="link-3" data_page="drip-series-paintings" href="#">Drip Series</a>
     <a class="link-3" data_page="black-and-white-paintings" href="#">Black and White</a>
     <a class="link-3" data_page="project-series-paintings" href="#">Project Series</a>
