41a8f16d4c37bf07c42928c85ac4a933865af25b
anad-zeal <elza.dana@gmail.com>
2025-11-19 15:33:00 -0700

    fix: update stylesheet comments and remove unused slideshow.css link

--- includes/header.php
+++ includes/header.php
@@ -56,9 +56,8 @@ $life_title = "<h2 class='sub-title fade-title'>The Life of an Artist</h2>";
          href="https://fonts.googleapis.com/css2?family=Bonheur+Royale&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Noto+Serif+Display:ital,wght@0,100..900;1,100..900&display=swap"
          rel="stylesheet">
 
-     <!-- Stylesheets sssssss-->
+     <!-- Stylesheet-->
      <link rel="stylesheet" href="/assets/css/style.css" />
-     <link rel="stylesheet" href="/assets/css/slideshow.css" />
 
  </head>
 
