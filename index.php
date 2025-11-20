3be0e0e5e67cf0cfc225b57a0aae17cb4dea725a
anad-zeal <elza.dana@gmail.com>
2025-11-20 15:41:04 -0700

    fix: add transparent color variable and update font links in index.php

--- index.php
+++ index.php
@@ -6,8 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Gallery Slideshow</title>
     <link rel="stylesheet" href="assets/css/style.css" />
-    <!-- Google Font for styling -->
-    <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600&display=swap" rel="stylesheet" />
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Bonheur+Royale&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Noto+Serif+Display:ital,wght@0,100..900;1,100..900&display=swap"
+        rel="stylesheet">
 </head>
 
 <body>
