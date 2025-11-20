bbc814d5371aeb582e89160373a3e4815d7abcbe
anad-zeal <elza.dana@gmail.com>
2025-11-20 16:04:16 -0700

    fix: reorder menu buttons for improved organization and consistency

--- includes/menu.php
+++ includes/menu.php
@@ -1,9 +1,10 @@
 <!-- IMPORTANT: The 'data_page' must match your folder names exactly -->
 <nav id="folder-selector">
-    <button class="col link-3 menu-button" data_page="decorative-painting">Decorative</button>
+
     <button class="col link-3 menu-button" data_page="historic-preservation">Historic Preservation</button>
     <button class="col link-3 menu-button" data_page="encaustic-paintings">Encaustic Paintings</button>
     <button class="col link-3 menu-button" data_page="black-and-white-paintings">Black &amp; White</button>
     <button class="col link-3 menu-button" data_page="drip-series-paintings">Drips</button>
     <button class="col link-3 menu-button" data_page="project-series-paintings">Project Series</button>
+    <button class="col link-3 menu-button" data_page="decorative-painting">Decorative</button>
 </nav>
