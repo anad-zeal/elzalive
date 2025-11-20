43e97c1ce5867d03edad2245fcf2284d692c2c85
anad-zeal <elza.dana@gmail.com>
2025-11-20 14:15:13 -0700

    refactor: remove unused gallery title and header include in index.php

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 </head>
 
 <body>
-    <?php include "includes/header.php"; ?>
+    <!-- <?php include "includes/header.php"; ?> -->
     <?php include "includes/menu.php"; ?>
     <?php include "includes/contents.php"; ?>
     <?php include "includes/footer.php"; ?>
