de0af72d2768517e21b8f59e9d99cf9fb307bd68
anad-zeal <elza.dana@gmail.com>
2025-11-20 16:18:10 -0700

    fix: update button data-page attributes for consistency

--- includes/menu.php
+++ includes/menu.php
@@ -1,9 +1,8 @@
 <nav id="folder-selector" aria-label="Gallery categories">
     <button class="col link-3 menu-button" data-page="decorative-painting">Decorative</button>
-    <button class="col link-3 menu-button" data-page="historic-preservation">Historic Preservation</button>
     <button class="col link-3 menu-button" data-page="encaustic-paintings">Encaustic Paintings</button>
     <button class="col link-3 menu-button" data-page="black-and-white-paintings">Black &amp; White</button>
     <button class="col link-3 menu-button" data-page="drip-series-paintings">Drips</button>
     <button class="col link-3 menu-button" data-page="project-series-paintings">Project Series</button>
-    <button class="col link-3 menu-button" data-page="restoration-projects">Restoration</button>
+    <button class="col link-3 menu-button" data-page="historic-preservation">Restoration</button>
 </nav>
