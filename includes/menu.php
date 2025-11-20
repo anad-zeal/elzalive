95c30a3e24650824be7eed35774840eb8d4b2564
anad-zeal <elza.dana@gmail.com>
2025-11-20 14:11:11 -0700

    fix: correct data_page attribute for Historic Preservation button in menu.php

--- includes/menu.php
+++ includes/menu.php
@@ -1,7 +1,7 @@
 <!-- IMPORTANT: The 'data_page' must match your folder names exactly -->
 <nav id="folder-selector">
     <button class="col link-3 menu-button btn" data_page="decorative-painting">Decorative</button>
-    <button class="col link-3 menu-button btn" data_page="erestoration-projects">Historic Preservation</button>
+    <button class="col link-3 menu-button btn" data_page="historic-preservation">Historic Preservation</button>
     <button class="col link-3 menu-button btn" data_page="encaustic-paintings">Encaustic Paintings</button>
     <button class="col link-3 menu-button btn" data_page="black-and-white-paintings">Black &amp; White</button>
     <button class="col link-3 menu-button btn" data_page="drip-series-paintings">Drips</button>
