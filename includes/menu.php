6b6e482f72fd1b353ee0744ee0128ce6eef9118b
anad-zeal <elza.dana@gmail.com>
2025-11-20 15:47:28 -0700

    fix: remove unnecessary 'btn' class from menu buttons in menu.php

--- includes/menu.php
+++ includes/menu.php
@@ -1,9 +1,9 @@
 <!-- IMPORTANT: The 'data_page' must match your folder names exactly -->
 <nav id="folder-selector">
-    <button class="col link-3 menu-button btn" data_page="decorative-painting">Decorative</button>
-    <button class="col link-3 menu-button btn" data_page="historic-preservation">Historic Preservation</button>
-    <button class="col link-3 menu-button btn" data_page="encaustic-paintings">Encaustic Paintings</button>
-    <button class="col link-3 menu-button btn" data_page="black-and-white-paintings">Black &amp; White</button>
-    <button class="col link-3 menu-button btn" data_page="drip-series-paintings">Drips</button>
-    <button class="col link-3 menu-button btn" data_page="project-series-paintings">Project Series</button>
+    <button class="col link-3 menu-button" data_page="decorative-painting">Decorative</button>
+    <button class="col link-3 menu-button" data_page="historic-preservation">Historic Preservation</button>
+    <button class="col link-3 menu-button" data_page="encaustic-paintings">Encaustic Paintings</button>
+    <button class="col link-3 menu-button" data_page="black-and-white-paintings">Black &amp; White</button>
+    <button class="col link-3 menu-button" data_page="drip-series-paintings">Drips</button>
+    <button class="col link-3 menu-button" data_page="project-series-paintings">Project Series</button>
 </nav>
