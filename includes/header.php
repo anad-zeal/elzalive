4704624c3d50303fd49552d876357ede3710c624
anad-zeal <elza.dana@gmail.com>
2025-11-25 19:17:44 -0700

    Update card grid dimensions and aspect ratio for improved layout

--- includes/header.php
+++ includes/header.php
@@ -53,7 +53,7 @@ $life_title = "<h2 class='sub-title fade-title'>The Life sof an Artist</h2>";
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link
-        href="https://fonts.googleapis.com/css2?family=Bonheur+Royale&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Noto+Serif+Display:ital,wght@0,100..900;1,100..900&display=swap"
+        href="https://fonts.googleapis.com/css2?family=Bonheur+Royale&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Noto+Serif+Display:ital,wght@0,100..900;1,100..900&family=Raleway:ital,wght@0,100..900;1,100..900&display=swap"
         rel="stylesheet">
 
     <!-- Stylesheets -->
