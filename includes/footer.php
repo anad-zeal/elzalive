018c6144302483a384a9f4af8df8afde2b35ccca
anad-zeal <elza.dana@gmail.com>
2025-12-01 15:19:54 -0700

    fix: Update footer text to correct 'MORE' to 'LESS'

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,5 @@
 <footer class="site-footer">
-    <p>&copy; 2025 elzalive • All rights reserved. MORE</p>
+    <p>&copy; 2025 elzalive • All rights reserved. LESS</p>
 </footer>
 
 <script src="assets/js/script.js" defer></script>
