0e7c1807af58e8010f28fca277eae67b2b11f5c3
anad-zeal <elza.dana@gmail.com>
2025-12-01 22:02:46 -0700

    fix: Ensure menu.php is included for proper navigation functionality

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 #Comment
+require __DIR__ . "/includes/menu.php";
 require __DIR__ . "/includes/header.php";
 require __DIR__ . "/includes/content.php";
 require __DIR__ . "/includes/footer.php";
