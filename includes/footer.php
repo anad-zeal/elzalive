2922277d9c1c6a05c72991852111a91b7a24176e
anad-zeal <elza.dana@gmail.com>
2025-12-08 16:58:03 -0700

    Delete all files

--- includes/footer.php
+++
@@ -1,9 +0,0 @@
-<footer class="site-footer">
-    <p>&copy; 2025 elzalive • All rights reserved. LESS</p>
-</footer>
-
-<script src="assets/js/script.js" defer></script>
-
-</body>
-
-</html>
--- index.php
+++
@@ -1,5 +0,0 @@
-<?php
-require __DIR__ . "/includes/menu.php";
-require __DIR__ . "/includes/header.php";
-require __DIR__ . "/includes/content.php";
-require __DIR__ . "/includes/footer.php";
